511a16cbc9b988a7d024e6eba140580033b3c5ab
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-27 00:56:32 +0200

    Added The contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,26 +1,26 @@
 <?php
-//
-//namespace App\Http\Controllers;
-//
-//use Illuminate\Http\Request;
-//use App\Models\Contact;
-//use App\Models\Company;
-//
-//class ContactController extends Controller
-//{
-//    public function index()
-//    {
-//        $companies = Company::orderBy('name')->pluck('name', 'id');
-//        $contacts=Contact::orderBy('first_name', 'ASC')->paginate(10);
-//        return view('contacts.index', compact('contacts','companies'));
-//    }
-//    public function create()
-//    {
-//        return view('contacts.create');
-//    }
-//    public function show($id)
-//    {
-//        $contact =  Contact::find($id);
-//        return view('contacts.show',compact('contact'));
-//    }
-//}
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Models\Company;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $companies = Company::orderBy('name')->pluck('name', 'id');
+        $contacts=Contact::orderBy('first_name', 'ASC')->paginate(10);
+        return view('contacts.index', compact('contacts','companies'));
+    }
+    public function create()
+    {
+        return view('contacts.create');
+    }
+    public function show($id)
+    {
+        $contact =  Contact::find($id);
+        return view('contacts.show',compact('contact'));
+    }
+}
