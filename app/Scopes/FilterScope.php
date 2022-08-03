48e50dd7690baa0e264a098e4623ca9eafdfa2dd
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-03 04:52:31 +0200

    Using the global scope to add filter and advanced search options

--- app/Scopes/FilterScope.php
+++ app/Scopes/FilterScope.php
@@ -1,18 +1,18 @@
 <?php
-//
-//namespace App\Scopes;
-//use Illuminate\Database\Eloquent\Model;
-//use Illuminate\Database\Eloquent\Scope;
-//use Illuminate\Database\Eloquent\Builder;
-//
-//class FilterScope implements Scope
-//{
-//
-//    public function apply(Builder $builder, Model $model)
-//    {
-//        if($companyId = request('company_id')){
-//            $builder->where('company_id', $companyId);
-//        }
-//
-//    }
-//}
+
+namespace App\Scopes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Builder;
+
+class FilterScope implements Scope
+{
+
+    public function apply(Builder $builder, Model $model)
+    {
+        if($companyId = request('company_id')){
+            $builder->where('company_id', $companyId);
+        }
+
+    }
+}
