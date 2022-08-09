f26b5df3993fd14a3248faa7bde887dff7afa8ef
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-10 01:27:56 +0200

    Bug fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,7 +30,7 @@
             @auth
             <ul class="navbar-nav">
                 <li class="nav-item"><a href="#" class="nav-link">Companies</a></li>
-                <li class="nav-item active"><a href="{{route('contact.index')}}" class="nav-link">Contacts</a></li>
+                <li class="nav-item active"><a href="{{route('contacts.index')}}" class="nav-link">Contacts</a></li>
             </ul>
             @endauth
             <ul class="navbar-nav ml-auto">
