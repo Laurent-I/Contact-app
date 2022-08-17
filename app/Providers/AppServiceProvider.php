2b708797afab6c26efbda093250100cb376b7424
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-17 16:37:35 +0200

    Debugging our queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +26,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Illuminate\Pagination\Paginator::useBootstrap();
+
+//        if (env('APP_DEBUG')){
+//            DB::listen(function($query){
+//                Log::info($query->sql, $query->bindings, $query->time);
+//            });
+//        }
+
     }
 }
