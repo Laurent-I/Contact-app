f13eb251267de3f25227057912c476ef125e9108
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-17 17:51:51 +0200

    ÂAdded more columns for profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Company::class);
     }
+
+    public function fullName()
+    {
+        return $this->first_name . " ". $this->last_name;
+    }
 }
